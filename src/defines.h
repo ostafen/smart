b6d6180fceafd9066df3daa93e085e5a4ec2b483
matt <mattpalms@gmail.com>
2022-12-08 14:10:20 +0000

    Rearrange constants, add new ones and adjust.
      * Move constants into better categories.
      * Add new constant MAX_FAILURE_MESSAGES for test.
      * Make max algo name just 24 - 32 was too big for any real algo name.
      * Add NUM_PATTERNS_AT_END_OF_TEXT to define how many copies of a pattern can be written to at the end of the text.

--- src/defines.h
+++ src/defines.h
@@ -9,10 +9,20 @@
 #define MEGA_BYTE (1024 * 1024)           // constant for 1 MB size
 
 /*
- * Path and algorithm string length limits.
+ * Limits.
  */
 #define MAX_PATH_LENGTH 2048              // Maximum file path length.
-#define ALGO_NAME_LEN 33                  // Maximum length of an algorithm name string + 1 for null terminator char.
+#define ALGO_NAME_LEN 25                  // Maximum length of an algorithm name string + 1 for null terminator char.
+#define MAX_DATA_SOURCES 32               // max number of algorithm name/regexes eo specify on command line.
+#define MAX_SEARCH_PATHS 32               // maximum search paths for data or algo_names set via environment variable.
+#define MAX_SELECT_ALGOS 2048             // maximum number of algorithms which can be selected for benchmarking.
+#define MAX_DATA_FILES 128                // max number of different files to load to fill the benchmark text buffer.
+#define NUM_PATTERNS_MAX 100              // maximum number of different pattern lengths to benchmark at one time.
+#define MAX_LINE_LEN 128                  // max length of line to output to console.
+#define STR_BUF 256                       // general strings with a bit of space to spare - parameters, filenames, etc.
+#define NUM_PATTERNS_AT_END_OF_TEXT 3     // Number of pattern-lengths to add to the text buffer so algorithms that write
+                                          // to the end of the text (e.g. sentinel guard) do not cause buffer overflows.
+#define TEXT_SIZE_PADDING 256             // extra memory allocated to end of text buffer for safety in case algorithms modify too much text.
 
 /*
  * Default file and directory names.
@@ -36,32 +46,21 @@
 #define SEARCH_PATH_DELIMITER ":"                              // delimiter used to separate search paths specified in environment variables.
 
 /*
- * Maximum algorithms and data sources which can be specified.
- */
-#define MAX_DATA_SOURCES 32               // max number of algorithm name/regexes to specify on command line.
-#define MAX_SEARCH_PATHS 32               // maximum search paths for data or algo_names set via environment variable.
-#define MAX_SELECT_ALGOS 2048             // maximum number of algorithms which can be selected for benchmarking.
-#define MAX_DATA_FILES 128                // max number of different files to load to fill the benchmark text buffer.
-
-/*
  * Benchmarking defaults
  */
 #define TEXT_SIZE_DEFAULT 1048576         // default size of text buffer for benchmarking
-#define TEXT_SIZE_PADDING 256             // extra memory allocated to end of text buffer for safety in case algorithms modify too much text.
 #define PATTERN_MIN_LEN_DEFAULT 2         // default minimum size of pattern to benchmark.
 #define PATTERN_MAX_LEN_DEFAULT 4096      // default maximum size of pattern to benchmark.
-#define NUM_PATTERNS_MAX 100              // maximum number of different pattern lengths to benchmark at one time.
 #define NUM_RUNS_DEFAULT 500              // default number of patterns of a given size to benchmark with.
 #define TIME_LIMIT_MILLIS_DEFAULT 300     // default time limit in milliseconds for a search to pass benchmarking.
 #define CPU_PIN_DEFAULT PIN_LAST_CPU      // default CPU pinning - can be [off | last | {digit}]
 
 /*
- * Test defaults
+ * Test settings
  */
 #define TEST_TEXT_SIZE 8192               // size of text buffer for testing.
 #define TEST_PATTERN_MAX_LEN 2048         // max length of random patterns to use when testing.
-#define NUM_RANDOM_PATTERN_TESTS 16       // number of different random patterns to test for each algo and alphabet size.
-
+#define MAX_FAILURE_MESSAGES 32           // max number of failure messages to record.
 /*
  * Console output formatting defines.
  */
@@ -71,8 +70,6 @@
 /*
  * Miscellaneous string defines.
  */
-#define MAX_LINE_LEN 128                  // max length of line to output to console.
-#define STR_BUF 256                       // general strings with a bit of space to spare - parameters, filenames, etc.
 #define STR_END_CHAR '\0'                 // zero char to terminate strings.
 
 #endif //SMART_DEFINES_H
