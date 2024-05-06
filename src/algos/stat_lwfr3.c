66600a9996376a3850299040475eb6cd9023e7d8
matt <mattpalms@gmail.com>
2024-05-06 15:29:35 +0100

    Instrumented algorithms
     * Implementations of various algorithms instrumented to collect run-time statistics on:
    
      * text bytes read
      * pattern bytes read
      * number writes
      * number branches
      * number computations
      * number lookups
      * number verifications
      * number jumps
      * three custom extra fields for each algorithm to record specific information.

---
+++ src/algos/stat_lwfr3.c
@@ -0,0 +1,193 @@
+/*
+ *	ALGORITHM Weak Factor Recognizer, Linear Version, (LWFR)
+ *  By Simone Faro, Domenico Cantone and Arianna Pavone
+ *
+ * PREPROCESSING:
+ *		an hash value is computed for al factors of the pattern with length in [1..16]
+ *		the computed hash value is always a number in [0...256*256]
+ *		if w is a factor of x, and hash(w) is its hash value, than F[hash(w)]=TRUE, otherwise F[hash(w)]=FALSE
+ * SEARCHING
+ *		The algorithm searches for factors of the pattern using a weak recognition method
+ *		the search phase is very similar to BOM.
+ *		The window is scanned right to left and for each character a new hash value of the suffix of the window is computed.
+ *		Let w be the suffix we scanned. If F[hash(w)]=TRUE we continue scanning the next character of the window.
+ *		Otherwise we stop scanning (w is not a factor of the pattern) and jump to the right, like in BOM.
+ *      Linearity is obtained by combaining the veification phase of the KMP algorithm
+ */
+
+
+#include "include/define.h"
+#include "include/main.h"
+#define Q 3
+#define HASH(j) (y[j]<<4) + (y[j-1]<<2) + y[j-2]
+
+void preKmp(unsigned char *x, int m, int kmpNext[]) {
+    int i, j;
+    i = 0;
+    j = kmpNext[0] = -1;
+    while (i < m) {
+        while (j > -1 && x[i] != x[j])
+            j = kmpNext[j];
+        i++;
+        j++;
+        if (i<m && x[i] == x[j])
+            kmpNext[i] = kmpNext[j];
+        else
+            kmpNext[i] = j; // i == m, requires m + 1 elements in kmpNext.
+    }
+}
+
+void preprocessing(unsigned char *x, int m, char *F) {
+    int i,j;
+    unsigned short h;
+    int fact = m<16?m:16;
+    //for(i=0; i<256*256; i++) F[i] = FALSE;
+    long long int *G = (long long int *) F;
+    int K = 65536 / sizeof(long long int);
+    for(i=0; i<K; i++) G[i] = FALSE;
+    for(i=0; i<m; i++) {
+        int stop = (i-fact+1)>0?(i-fact+1):0;
+        h = 0;
+        for(j=i; j>=stop; j--) {
+            h = h<<2;
+            h += x[j];
+            F[h] = TRUE;
+        }
+    }
+}
+
+
+int search(unsigned char *x, int m, unsigned char *y, int n) {
+    int i, j, p, b, lf, count, test, kmpNext[m + 1];
+    int tp, st;
+    char F[256*256];
+    unsigned short h;
+    if(m<Q) return -1;
+    
+    BEGIN_PREPROCESSING
+    /* Preprocessing */
+    preKmp(x, m, kmpNext);
+    int plen = m;
+    if(m%Q!=0) m = m-(m%Q);
+    int mm1 = m-1;
+    int mq = m-Q+1;
+    preprocessing(x,m,F);
+    for(i=0; i<m; i++) y[n+i]=x[i];
+    END_PREPROCESSING
+    
+    BEGIN_SEARCHING
+    /* Searching */
+    count = 0;
+    tp = 0;
+    st = 0;
+    _stats.num_branches++;
+    if (stats_verify_pattern(0, x, plen, y, n) == plen) count++;
+    j = m;
+    _stats.num_jumps++; // advances one.
+    _stats.num_writes += 4; // store value.
+    _stats.num_branches++;
+    while (j < n) {
+        h = HASH(j);
+        _stats.num_writes++; // store value.
+        _stats.num_computations++;
+        _stats.text_bytes_read += Q;
+
+        _stats.num_lookups++; // for the first time around the loop F[h]
+        _stats.num_branches++;
+        while (!F[h]) {
+            j += mq;
+            _stats.num_writes++; // store value.
+            _stats.num_jumps++;
+
+            h = HASH(j);
+            _stats.num_writes++; // store value.
+            _stats.text_bytes_read += Q;
+            _stats.num_computations++;
+            _stats.num_lookups++; // for the next time around the loop F[h].
+            _stats.num_branches++;
+        }
+        lf = b = j - m + Q;
+        _stats.num_writes++; // store value.
+        _stats.num_branches++;
+        if (b < tp) {
+            b = tp - 1;  //b is the maximum between lf and tp-1
+            _stats.num_writes++; // store value.
+        }
+
+        _stats.num_lookups++; // F[h] for the first loop iteration.
+        _stats.num_branches++;
+        while ((test = F[h]) && j > b) {
+            j -= Q;
+            _stats.num_writes++; // store value.
+
+            h = (h << 6) + HASH(j);
+            _stats.num_writes++; // store value.
+            _stats.text_bytes_read += Q;
+            _stats.num_computations++;
+            _stats.num_lookups++; // F[h] for the next loop iteration.
+            _stats.num_branches++;
+        }
+
+        // verification
+        _stats.num_branches++;
+        if (j <= b && test) {
+            _stats.num_verifications++;
+
+            lf -= Q - 1;
+            _stats.num_writes++; // store value.
+            _stats.num_branches++;
+            if (lf > tp) {
+                //restart kmp aut
+                tp = lf;
+                st = 0;
+                _stats.num_writes += 2; // store value.
+            }
+            _stats.num_branches++;
+            while (st >= tp - lf) {
+                _stats.num_branches++;
+                if (st < plen) {
+                    _stats.pattern_bytes_read++;
+                    _stats.text_bytes_read++;
+                }
+
+                _stats.num_branches++;
+                while (st < plen && x[st] == y[tp]) {
+                    st++;
+                    tp++;
+                    _stats.num_writes += 2; // store value.
+                    _stats.num_branches++;
+                    if (st < plen) {
+                        _stats.pattern_bytes_read++;
+                        _stats.text_bytes_read++;
+                    }
+                    _stats.num_branches++;
+                }
+
+                _stats.num_branches++;
+                if (st == plen && lf <= n - plen) count++;
+                st = kmpNext[st];
+                _stats.num_writes ++; // store value.
+                _stats.num_lookups++;
+                _stats.num_branches++;
+                if (st < 0) {
+                    st++;
+                    tp++;
+                    _stats.num_writes += 2; // store value.
+                }
+
+                _stats.num_branches++;
+            }
+            j = tp + mm1 - st;
+            _stats.num_writes++; // store value.
+            _stats.num_jumps++;
+        } else {
+            j += m - Q + 1;
+            _stats.num_writes++; // store value.
+            _stats.num_jumps++;
+        }
+        _stats.num_branches++;
+    }
+    END_SEARCHING
+    return count;
+}
+
---
+++ src/algos/stat_qf34.c
@@ -0,0 +1,117 @@
+/*
+ * SMART: string matching algorithms research tool.
+ * Copyright (C) 2012  Simone Faro and Thierry Lecroq
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ * 
+ * contact the authors at: faro@dmi.unict.it, thierry.lecroq@univ-rouen.fr
+ * download the tool at: http://www.dmi.unict.it/~faro/smart/
+ *
+ * This is an implementation of the QF (Q-gram Filtering) algorithm
+ * in Branislav Durian1, Hannu Peltola, Leena Salmela and Jorma Tarhio2 	
+ * Bit-Parallel Search Algorithms for Long Patterns
+ * International Symposium on Experimental Algorithms (SEA 2010)
+ * Q is the dimension of q-grams
+ */
+
+#include "include/define.h"
+#include "include/main.h"
+#define	Q	3
+#define	S	4
+
+#define ASIZE (1<<(Q*S))
+#define AMASK (ASIZE-1)
+#define BSIZE 262144	/* = 2**18 */
+
+int search(unsigned char *x, int m, unsigned char *y, int n)
+{
+	int count = 0;
+	int i, j, k, mq1=m-Q+1, B[ASIZE];
+	unsigned int D, ch, mask=AMASK;
+	if(m <= Q) return -1;
+	if((WORD*8) < Q) return -1;
+	if(ASIZE > BSIZE)	return -1;
+	
+	/* Preprocessing */
+   BEGIN_PREPROCESSING
+	for(i=0; i<ASIZE; i++) B[i]=0;		
+	ch = 0;
+	for(i = m-1; i >= 0; i--) {
+		ch = ((ch << S) + x[i]) & mask;
+		if(i < mq1)
+			B[ch] |= (1<<((m-i) % Q));
+	}
+   END_PREPROCESSING
+	
+	/* Searching */
+   BEGIN_SEARCHING
+   _stats.num_writes++;
+    _stats.num_branches++;
+    for(i=mq1-1; i<=n-Q; i+=mq1, _stats.num_writes++, _stats.num_jumps++) {
+		ch = y[i+2];
+		ch = (ch<<S) + y[i+1];
+		ch = (ch<<S) + y[i];
+        _stats.text_bytes_read += Q;
+        _stats.num_computations++;
+        _stats.num_writes++;
+
+        D = B[ch & mask];
+        _stats.num_writes++;
+        _stats.num_lookups++;
+        _stats.num_branches++;
+        if( D ) {
+            _stats.extra[0]++; // num times match on main loop.
+		   j = i-mq1+Q;
+           _stats.num_writes++;
+
+	      more:
+		   i = i-Q;
+            _stats.num_writes++;
+            _stats.num_branches++;
+            if(i >= j) {
+				ch = y[i+2];
+				ch = (ch<<S) + y[i+1];
+				ch = (ch<<S) + y[i];
+                _stats.text_bytes_read += Q;
+                _stats.num_computations++;
+                _stats.num_writes++;
+
+				D &= B[ch & mask];
+                _stats.num_writes++;
+                _stats.num_lookups++;
+                _stats.num_branches++;
+                if(D == 0) continue;
+		      else goto more;
+			} 
+		   else {  /* verify potential matches */
+			   i = j;
+                _stats.num_writes++;
+                k = j-Q+1;
+                _stats.num_writes++;
+                _stats.num_branches++;
+                if(j > n-m)  {
+                    j = n-m;
+                    _stats.num_writes++;
+                }
+                _stats.num_branches++;
+                for(  ; k <= j; k++) {
+                    _stats.num_branches++;
+                    if (stats_verify_pattern(k, x, m, y, n) == m) count++;
+                    _stats.num_branches++;
+                }
+		   }
+	   }
+       _stats.num_branches++;
+    }
+   END_SEARCHING
+	return count;
+}
