851f4473856472080fc489f2536edefb6e4b9768
matt <mattpalms@gmail.com>
2024-05-15 15:12:13 +0100

    Add algo stats metadata to record extra names.
     * Also improve stats gathering for verify function.
     * provide utility method to set an extra name.
     * Pass metadata into search function and copy results back.

--- src/algos/include/main.h
+++ src/algos/include/main.h
@@ -28,7 +28,8 @@
 
 /* global variables used for computing preprocessing and searching times */
 double _search_time, _pre_time; // searching time and preprocessing time
-algo_stats_t _stats;
+algo_stats_t _stats;            // runtime statistics for stat_ versions of algorithms
+algostats_metadata_t _metadata; // names of any extra runtime statistics fields returned by algorithms.
 TIMER timer;
 
 int search(unsigned char *x, int m, unsigned char *y, int n);
@@ -58,7 +59,7 @@ int search(unsigned char *x, int m, unsigned char *y, int n);
  * Intended to replace usages of memcmp, or other naive verification methods that compare the pattern bytes to the text,
  * while updating stats correctly.
  *
- *  Note: it will not replicate more complex pattern matching, or record any branching that takes place.
+ *  Note: it will not replicate more complex pattern matching.
  *
  * Returns the number of characters matched at position pos.
  *
@@ -69,20 +70,31 @@ int stats_verify_pattern(int pos, unsigned char *x, int m, unsigned char *y, int
 {
     _stats.num_verifications++;
     int charpos = 0;
+    _stats.num_branches++;
     while (charpos < m && pos + charpos < n) {
         _stats.pattern_bytes_read++;
         _stats.text_bytes_read++;
+        // we don't record this as a branch, because this would normally be done in the outer while, which is already counted.
+        // it's split out here in order that we can record the text and pattern bytes read first.
         if (x[charpos] != y[pos + charpos]) break;
         charpos++;
+        _stats.num_branches++;
     }
     return charpos;
 }
 
+void set_extra_name(char *name, int index)
+{
+    snprintf(_metadata.extra_name[index], EXTRA_FIELD_NAME_LEN, "%s", name);
+}
+
 int internal_search(unsigned char *x, int m, unsigned char *y, int n,
-                    double *pre_time, double *search_time, algo_stats_t *algo_stats)
+                    double *pre_time, double *search_time, algo_stats_t *algo_stats, algostats_metadata_t *metadata)
 {
     // Init measurement stats:
     init_stats(&_stats);
+    init_metadata(&_metadata);
+
     _search_time = 0.0;
     _pre_time = 0.0;
 
@@ -92,7 +104,8 @@ int internal_search(unsigned char *x, int m, unsigned char *y, int n,
     // Record measurement stats.
     *search_time = _search_time;
     *pre_time = _pre_time;
-    memcpy(algo_stats, &_stats, sizeof(*algo_stats));
+    memcpy(algo_stats, &_stats, sizeof(algo_stats_t));
+    memcpy(metadata, &_metadata, sizeof(algostats_metadata_t));
 
     // Return occurrences or error value (if negative).
     return occ;
